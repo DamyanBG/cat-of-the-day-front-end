2642ec462688992ce70f86ee39a35fa3600e6606
DamyanBG <d.dimitrov.1988@abv.bg>
2023-04-04 00:00:50 +0300

    change to azure host

--- src/components/common/urls.ts
+++ src/components/common/urls.ts
@@ -1,3 +1,5 @@
-const LOCAL_HOST = "http://127.0.0.1:5000"
+// const LOCAL_HOST = "http://127.0.0.1:5000"
 
-export const HOST_URL = LOCAL_HOST
+const AZURE_HOST = "https://cat-of-the-day-back-end.azurewebsites.net/"
+
+export const HOST_URL = AZURE_HOST
