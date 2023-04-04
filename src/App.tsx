13d5188e0a73cd580e7de6515d75f70e76718395
DamyanBG <d.dimitrov.1988@abv.bg>
2023-04-04 12:44:12 +0300

    add text

--- src/App.tsx
+++ src/App.tsx
@@ -9,30 +9,32 @@ import Rules from './components/rules/Rules';
 import { UserProvider } from './components/context/UserContext';
 import Vote from './components/cat/Vote';
 import CatReview from './components/cat/CatReview';
+import { CatExistsProvider } from './components/context/CatExistsContext';
 
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
-
 
 function App() {
   return (
     <div className="App">
       <UserProvider>
-        <BrowserRouter>
-          <Header />
-          <main className="p-4">
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/register" element={<Register />} />
-              <Route path="/login" element={<LoginVoter />} />
-              <Route path="/upload-cat" element={<UploadCat />} />
-              <Route path="/rules" element={<Rules />} />
-              <Route path="/vote" element={<Vote />} />
-              <Route path="/cat-review" element={<CatReview />} />
-            </Routes>
-          </main>
-          <Footer />
-        </BrowserRouter>
+        <CatExistsProvider>
+          <BrowserRouter>
+            <Header />
+            <main className="p-4">
+              <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/login" element={<LoginVoter />} />
+                <Route path="/upload-cat" element={<UploadCat />} />
+                <Route path="/rules" element={<Rules />} />
+                <Route path="/vote" element={<Vote />} />
+                <Route path="/cat-review" element={<CatReview />} />
+              </Routes>
+            </main>
+            <Footer />
+          </BrowserRouter>
+        </CatExistsProvider>
       </UserProvider>
     </div>
   );
--- src/components/context/CatExistsContext.tsx
+++ src/components/context/CatExistsContext.tsx
@@ -1,5 +1,39 @@
-import React, { createContext } from "react";
+import React, { createContext, useEffect, useState } from "react";
+
+type Props = { children: React.ReactNode }
 
 type CatExists = boolean
 
-export const CatExistsContext: React.Context<boolean> = createContext<boolean>(false)
+interface CatExistsContextType {
+    catExists: CatExists;
+    setCatExists: React.Dispatch<React.SetStateAction<CatExists>>;
+  }
+
+export const CatExistsContext = createContext<CatExistsContextType>({
+    catExists: false,
+    setCatExists: () => {}
+})
+
+export const CatExistsProvider: React.FC<Props> = ({ children }) => {
+    const [catExists, setCatExists] = useState<CatExists>(false)
+
+    useEffect(() => {
+        const localStorageCatExists = localStorage.getItem("catExist");
+    
+        if (localStorageCatExists) {
+          const localCatExists: CatExists = JSON.parse(localStorageCatExists)
+          setCatExists(localCatExists);
+        }
+      }, []);
+
+      return (
+        <CatExistsContext.Provider
+          value={{
+            catExists,
+            setCatExists,
+          }}
+        >
+          {children}
+        </CatExistsContext.Provider>
+      );
+}
