ccbe80b2ac4d35262b31ec403790a91a199b2af2
DamyanBG <d.dimitrov.1988@abv.bg>
2023-07-14 14:07:35 +0300

    remove pipeline and finish logout

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,7 +3,7 @@ import { Navbar, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { UserContext } from "../context/UserContext";
 import { CatExistsContext } from "../context/CatExistsContext";
-import { logOut } from "../users/logout";
+import { LogOut } from "../users/LogOut";
 
 const Header: React.FC = () => {
   const { user } = useContext(UserContext);
@@ -40,9 +40,7 @@ const Header: React.FC = () => {
               <Nav.Link as={Link} to="/profile">
                 Profile
               </Nav.Link>
-              <Nav.Link onClick={logOut}>
-                Logout
-              </Nav.Link>
+              <LogOut />
             </>
           ) : (
             <>
--- src/components/users/LogOut.tsx
+++ src/components/users/LogOut.tsx
@@ -2,9 +2,9 @@ import { UserContext } from "../context/UserContext"
 import { useContext } from "react"
 import { useNavigate } from "react-router-dom"
 import { HOST_URL } from "../common/urls"
+import { Nav } from "react-bootstrap"
 
-
-export const logOut = () => {
+export const LogOut = () => {
     const { user, setUser } = useContext(UserContext)
     const navigate = useNavigate()
 
@@ -13,16 +13,36 @@ export const logOut = () => {
             method: "POST",
             body: JSON.stringify({
                 token: user.token
-            })
+            }),
+            headers: {
+                Authorization: `Bearer ${user.token}`,
+                "Content-Type": "application/json",
+            }
         })
             .then((resp) => {
+                if (resp.status === 400) {
+                    return resp.json()
+                }
+
                 if (resp.status === 201) {
                     setUser({})
                     window.localStorage.clear()
                     navigate("/")
                 }
+                return resp.json()
+            })
+            .then((json) => {
+                console.log(json)
             })
     }
 
-    postLogOut()
+    const handleOnLogOutClick = () => {
+        postLogOut()
+    }
+
+    return (
+        <Nav.Link onClick={handleOnLogOutClick}>
+            Logout
+        </Nav.Link>
+    )
 }
